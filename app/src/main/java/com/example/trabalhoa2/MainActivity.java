f3ceafa8f6a468a9261f4541a16d5622c2595532
Lu Romao <lrr.santiago@gmail.com>
2023-06-23 08:18:22 -0300

    Remoção tela não utilizada

--- app/src/main/java/com/example/trabalhoa2/MainActivity.java
+++ app/src/main/java/com/example/trabalhoa2/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_cadastro_clientes, R.id.nav_historico_alugueis, R.id.nav_reservas, R.id.nav_programa_milhagem, R.id.nav_missao, R.id.nav_sobre)
+                R.id.nav_home, R.id.nav_cadastro_clientes, R.id.nav_historico_alugueis, R.id.nav_reservas, R.id.nav_missao, R.id.nav_sobre)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
